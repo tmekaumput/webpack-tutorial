49e976ceff018438bf396a3d3d5bf7edf8292336
Top Mekaumput <kampanart_m@yahoo.com>
2018-07-27 10:13:24 +1000

    Add font-awesome, playing with font-awesome

---
+++ 11-fontawesome/app.js
@@ -0,0 +1,10 @@
+// styles
+require('./styles/main.scss');
+
+// scripts
+const api = require('./api.js');
+const welcomeUser = require('./welcomeuser.coffee');
+const reactApp = require('./react/ReactApp.js');
+
+//welcomeUser('John');
+//api.getRates('USR', rates => console.log(rates));
