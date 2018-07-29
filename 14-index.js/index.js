6cbc2fad073ae07e3bb775d165201f889e7f3d4f
Top Mekaumput <kampanart_m@yahoo.com>
2018-07-30 08:56:03 +1000

    14-index.js, get to know default js file for require and import

---
+++ 14-index.js/index.js
@@ -0,0 +1,10 @@
+// styles
+require('./styles/main.scss');
+
+// scripts
+const api = require('Api/users');
+const welcomeUser = require('./welcomeuser');
+const reactApp = require('./react/ReactApp');
+
+//welcomeUser('John');
+//api.getRates('USR', rates => console.log(rates));
